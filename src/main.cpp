b24a0808f43b30a3e5bcaa05280f91c63db3fbf0
honey-ants <anthony.sbudisuharto@gmail.com>
2025-02-26 22:14:32 +0800

    Completed buzzer testing. Speaker code WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
-#include <Arduino.h>
+/*
+Speaker Test
+19 (Blue) --> SU (Shutdown, not essential)
+20 (Purple) --> GAIN (Affects the decibel gain)
+21 (Grey) --> DIN (Data In)
+47 (White) --> BCLK (Board Clock)
+48 (Black) --> LRC (Left/Right Clock)
+*/
+#include "Arduino.h"
+#include "Audio.h"
+#include "WiFi.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define I2C_DOUT 21
+#define I2C_BCLK 47
+#define I2C_LRC 48
+Audio audio;
+String ssid = "early worm gets the worm";
+String password = "biarcepet";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid.c_str(), password.c_str());
+  while (WiFi.status() != WL_CONNECTED)
+  delay(1500);
+
+  Serial.begin(115200);
+  audio.setPinout(I2C_BCLK, I2C_LRC, I2C_DOUT);
+  audio.setVolume(50);
+  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/Pink-Panther.wav");
+  // audio.connecttoFS(SPIFFS, "/buzzer.wav");  
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Looped.");
+  audio.loop();
+  delay(2000);
 }
---
+++ testfiles/buttonTest.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+const int buttonPin = 10;
+const int ledPin = 11;
+int buttonState;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT_PULLDOWN);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Started");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Button is pressed.");
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    Serial.println("Button is not pressed.");
+  }
+  delay(100);
+}
